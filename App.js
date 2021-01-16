e497e8c3da33a8380397b7eb7337e50e9eeeace8
joft-ware <zxc91911003@gmail.com>
2021-01-17 00:43:20 +0900

    Ad Loading

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>신기하구만.@@@@@@@!</Text>
+      <Text>신기하구만.@@@@@@@!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+function Loading() {
+    return <View> </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1;
+    }
+})
