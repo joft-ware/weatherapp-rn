689e0b606a3658c7d109681648e8b2b1b883fac1
joft-ware <zxc91911003@gmail.com>
2021-01-18 18:58:07 +0900

    Add Weather.js

--- App.js
+++ App.js
@@ -1,24 +1,53 @@
 /** @format */
 
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import Weather from "./Weather";
+import * as Location from "expo-location";
+import axios from "axios";
+import cheerio from "cheerio";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>신기하구만.@@@@@@@!</Text>
-      <Text>신기하구만.@@@@@@@!</Text>
-      <StatusBar style='auto' />
-    </View>
-  );
+var number = 1000;
+
+const API_KEY = "445d7c93c775a24741fad3150783fbb8";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: "clear",
+      temp: data.main.temp,
+    });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "sad");
+    }
+  };
+
+  // getHTML 함수 실행 후 데이터에서
+  // body > main > div > section > ul > li > article > h2 > a
+  // 에 속하는 제목을 titleList에 저장
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
